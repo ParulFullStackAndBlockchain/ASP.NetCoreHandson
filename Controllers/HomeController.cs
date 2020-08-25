7422e00d489e52c54fb824fd942cc7334e6918b7
ParulTiwari <parul.tiwari17@gmail.com>
2020-08-25 19:26:16 +0530

    Model in ASP.NET Core MVC

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using EmployeeManagement.Models;
 
 namespace EmployeeManagement.Controllers
 {
     public class HomeController : Controller
     {
-        public JsonResult Index()
+        private IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
         {
-            return Json(new { id = 1, name = "pragim" });
+            _employeeRepository = employeeRepository;
+        }
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(1).Name;
         }
     }
 }
