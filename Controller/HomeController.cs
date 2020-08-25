107bcb55d49df224df9881094c84ba469208c0ab
ParulTiwari <parul.tiwari17@gmail.com>
2020-08-25 18:29:22 +0530

    Setup mvc in asp.net core

---
+++ Controller/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controller
+{
+    public class HomeController 
+    {
+        public string Index()
+        {
+            return "Hello from MVC";
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +27,10 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Step1: Adds the required MVC services to the dependency injection container in asp.net core
+            //Step2: Endpoint Routing does not support 'IApplicationBuilder.UseMvc(...)'. To use 'IApplicationBuilder.UseMvc'
+            //set 'MvcOptions.EnableEndpointRouting = false'
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,13 +44,16 @@ namespace EmployeeManagement
 
             app.UseStaticFiles();
 
+            //Step3 : Add MVC midddleware to our application's request processing pipeline.
+            app.UseMvcWithDefaultRoute();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hosting Environment: " + env.EnvironmentName);
+                    await context.Response.WriteAsync("Hello World");
                 });
             });
 
