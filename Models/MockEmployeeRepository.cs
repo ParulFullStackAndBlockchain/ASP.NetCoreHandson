95a935ce742ed5ddfaa4243c51a217231c089e7b
ParulTiwari <parul.tiwari17@gmail.com>
2020-08-26 19:30:34 +0530

    List view in asp.net core mvc

--- Models/MockEmployeeRepository.cs
+++ Models/MockEmployeeRepository.cs
@@ -19,6 +19,11 @@ namespace EmployeeManagement.Models
         };
         }
 
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return this._employeeList;
+        }
+
         public Employee GetEmployee(int Id)
         {
             return this._employeeList.FirstOrDefault(e => e.Id == Id);
