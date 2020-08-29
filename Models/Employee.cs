0dd3b86628252106b570313eac2509760d90a528
ParulTiwari <parul.tiwari17@gmail.com>
2020-08-29 19:17:04 +0530

    AddSingleton vs AddScoped vs AddTransient

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -15,10 +15,6 @@ namespace EmployeeManagement.Models
         [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",ErrorMessage = "Invalid email format")]
         [Required]
         public string Email { get; set; }
-        //Note : By default an enum underlying data type is int.
-        //Value types(such as int, float, decimal, DateTime) are inherently required and don't need the Required attribute.
-        //Select List Required Validation : If you make the Department property, a nullable property by including a question 
-        //mark then the[Required] attribute is needed to make the field a required field.
         [Required]
         public Dept? Department { get; set; }
     }
--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,18 @@ namespace EmployeeManagement
             //returned. If the Accept header is set to application/json, then JSON data is returned.
             services.AddMvc(option => option.EnableEndpointRouting = false).AddXmlSerializerFormatters();
 
+            //Creates a Singleton service. A Singleton service is created when it is first requested. This same instance is then 
+            //used by all the subsequent requests.So in general, a Singleton service is created only one time per application and 
+            //that single instance is used throughout the application life time.
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+
+            //Creates a Scoped service. A new instance of a Scoped service is created once per request within the scope.For example,
+            //in a web application it creates 1 instance per each http request but uses the same instance in the other calls within
+            //that same web request.
+            //services.AddScoped<IEmployeeRepository, MockEmployeeRepository>();
+
+            //Creates a Transient service. A new instance of a Transient service is created each time it is requested.
+            //services.AddTransient<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
