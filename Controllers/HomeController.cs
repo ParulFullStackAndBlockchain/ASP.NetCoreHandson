930e86edfca4df375688d2c563dd76fc282fa4cb
ParulTiwari <parul.tiwari17@gmail.com>
2020-08-31 19:20:17 +0530

    File upload in asp.net core mvc

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,21 +5,22 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using EmployeeManagement.Models;
 using EmployeeManagement.ViewModels;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace EmployeeManagement.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IEmployeeRepository _employeeRepository;
+        private readonly IHostingEnvironment hostingEnvironment;
 
-        //Inject IEmployeeRepository using Constructor Injection.This prevents HomeController from being coupled to 
-        //MockEmployeeRepository. Later if we provide a new implementation for IEmployeeRepository and if we want to use that 
-        //new implementation we can do the same very easily.So dependency injection prevents tight coupling.
-        public HomeController(IEmployeeRepository employeeRepository)
+        public HomeController(IEmployeeRepository employeeRepository,IHostingEnvironment hostingEnvironment)
         {
             _employeeRepository = employeeRepository;
+            this.hostingEnvironment = hostingEnvironment;
         }
-       
+
         public ViewResult Index()
         {
             var model = _employeeRepository.GetAllEmployees();
@@ -44,14 +45,45 @@ namespace EmployeeManagement.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Employee employee)
+        public IActionResult Create(EmployeeCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
-                Employee newEmployee = _employeeRepository.Add(employee);
-                //return RedirectToAction("details", new { id = newEmployee.Id });
+                string uniqueFileName = null;
+
+                // If the Photo property on the incoming model object is not null, then the user
+                // has selected an image to upload.
+                if (model.Photo != null)
+                {
+                    // The image must be uploaded to the images folder in wwwroot
+                    // To get the path of the wwwroot folder we are using the inject
+                    // HostingEnvironment service provided by ASP.NET Core
+                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                    // To make sure the file name is unique we are appending a new
+                    // GUID value and and an underscore to the file name
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    // Use CopyTo() method provided by IFormFile interface to
+                    // copy the file to wwwroot/images folder
+                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+
+                Employee newEmployee = new Employee
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Department = model.Department,
+                    // Store the file name in PhotoPath property of the employee object
+                    // which gets saved to the Employees database table
+                    PhotoPath = uniqueFileName
+                };
+
+                _employeeRepository.Add(newEmployee);
+                return RedirectToAction("details", new { id = newEmployee.Id });
             }
+
             return View();
         }
     }
 }
+    
---
+++ ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Dept? Department { get; set; }
+        //The file uploaded to the server can be accessed through Model Binding using the IFormFile interface.
+        public IFormFile Photo { get; set; }
+    }
+}
