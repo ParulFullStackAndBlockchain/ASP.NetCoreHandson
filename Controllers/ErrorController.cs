e67e564eac4041dbda482fbb8da6756c0481285b
ParulTiwari <parul.tiwari17@gmail.com>
2020-09-01 19:07:42 +0530

    UseStatusCodePagesWithRedirects vs UseStatusCodePagesWithReExecute

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers
@@ -14,10 +15,16 @@ namespace EmployeeManagement.Controllers
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
+            //Note: If you are using UseStatusCodePagesWithReExecute middleware, it's also possible to get the original 
+            //path in the ErrorController using IStatusCodeReExecuteFeature.
+            var statusCodeResult =  HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
             switch (statusCode)
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found";
+                    ViewBag.Path = statusCodeResult.OriginalPath;
+                    ViewBag.QS = statusCodeResult.OriginalQueryString;
                     break;
             }
 
